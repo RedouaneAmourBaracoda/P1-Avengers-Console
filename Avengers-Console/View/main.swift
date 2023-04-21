2c254810def3f0023046ef806049d7b2a721a1c1
RedouaneAmourBaracoda <redouane.amour@baracoda.com>
2023-04-21 21:22:43 +0200

    remove unecessary comments

--- Avengers-Console/View/main.swift
+++ Avengers-Console/View/main.swift
@@ -14,10 +14,6 @@ enum Initialize {
     case initializeWithAttakersOnly
 }
 
-// Modele MVC adapé à la console. Evoquer GitHub pour le versionning du projet.
-
-// Insérer dans la présentation les aspects pris en compte pour ce projet : GitHub, Single responsability principle, MVC Architecture, Swiftlint, protocole, extension, Error handling (do try catch)., utilisation d'optionnels
-
 let game = Game()
 game.generalInitialize(.initializeForReal) //  Game is initialized with teams. And returns to main.
 game.mainFight()
